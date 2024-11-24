778ec39b0a4f1a0ff0876e564069c89de1375163
Tsimir <forcollabdaniel@gmail.com>
2024-11-24 13:33:35 +0700

    de commento for previuz version plus crunch mod
    Ну тут я закомметил все приколы и добавил свап режима приседаний

--- Assets/Player/Scripts/Movement/CrouchController.cs
+++ Assets/Player/Scripts/Movement/CrouchController.cs
@@ -1,32 +1,60 @@
 using UnityEngine;
 
-[System.Serializable]
+[System.Serializable]  // Позволяет сериализацию класса для отображения в инспекторе Unity
 public class CrouchController
 {
-    [SerializeField] private float _crouchHeight = 1f;
-    [SerializeField] private float _standingHeight = 2f;
-    [SerializeField] private float _crouchTransitionSpeed = 5f;
+    [SerializeField] private float _crouchHeight = 1f;           // Высота персонажа в приседе
+    [SerializeField] private float _standingHeight = 2f;         // Высота персонажа в стоячем положении
+    [SerializeField] private float _crouchTransitionSpeed = 5f;  // Скорость перехода между положениями
+    [SerializeField] private float _typeOfCrunch = 0;
 
-    private CharacterController _characterController;
-    private bool _isCrouching;
+    private CharacterController _characterController;            // Ссылка на компонент CharacterController
+    private bool _isCrouching;                                    // Флаг, определяющий, находится ли персонаж в приседе
 
     public void Initialize(CharacterController characterController)
     {
+        // Сохраняем ссылку на CharacterController
         _characterController = characterController;
     }
 
     public void HandleCrouch()
     {
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            _isCrouching = true;
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftControl))
-        {
-            _isCrouching = false;
+        switch (_typeOfCrunch) // Выбор типа приседаний
+            { case 0:
+                // Если нажата клавиша Left Control, начинаем приседать
+                if (Input.GetKeyDown(KeyCode.LeftControl))
+                {
+                    _isCrouching = true;
+                }
+                // Если отпущена клавиша Left Control, встаем
+                else if (Input.GetKeyUp(KeyCode.LeftControl))
+                {
+                    _isCrouching = false;
+                }
+                break;
+                case 1:
+                // Если нажата клавиша Left Control, меняем положение
+                if (Input.GetKeyDown(KeyCode.LeftControl))
+                {
+                    _isCrouching = !_isCrouching;
+                }
+                break;
+
+
         }
 
+
+
+
+
+        // Устанавливаем целевую высоту в зависимости от состояния персонажа
         float targetHeight = _isCrouching ? _crouchHeight : _standingHeight;
-        _characterController.height = Mathf.Lerp(_characterController.height, targetHeight, Time.deltaTime * _crouchTransitionSpeed);
+
+        // Плавный переход высоты персонажа к целевой высоте
+        _characterController.height = Mathf.Lerp(
+            _characterController.height,
+            targetHeight,
+            Time.deltaTime * _crouchTransitionSpeed
+        );
     }
 }
--- Assets/Player/Scripts/Movement/PlayerController.cs
+++ Assets/Player/Scripts/Movement/PlayerController.cs
@@ -1,40 +1,57 @@
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(CharacterController))]  // Требует наличия компонента CharacterController
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private MovementController _movementController;
-    [SerializeField] private JumpController _jumpController;
-    [SerializeField] private CrouchController _crouchController;
+    [SerializeField] private MovementController _movementController;  // Контроллер движений
+    [SerializeField] private JumpController _jumpController;          // Контроллер прыжков
+    [SerializeField] private CrouchController _crouchController;      // Контроллер приседаний
 
-    private CharacterController _characterController;
-    private Vector3 _velocity;
-    private bool _isGrounded;
+    private CharacterController _characterController;                 // Ссылка на компонент CharacterController
+    private Vector3 _velocity;                                         // Текущая скорость персонажа
+    private bool _isGrounded;                                          // Флаг, стоящий ли персонаж на земле
 
     private void Start()
     {
+        // Получаем ссылку на компонент CharacterController
         _characterController = GetComponent<CharacterController>();
+
+        // Инициализируем контроллеры движений и приседаний
         _movementController.Initialize(_characterController);
         _crouchController.Initialize(_characterController);
     }
 
     private void Update()
     {
+        // Определяем, стоит ли персонаж на земле
         _isGrounded = _characterController.isGrounded;
+
+        // Обрабатываем перемещение персонажа
         _movementController.Move(_isGrounded);
+
+        // Обрабатываем прыжок
         _jumpController.HandleJump(ref _velocity, _isGrounded);
+
+        // Обрабатываем приседание
         _crouchController.HandleCrouch();
+
+        // Применяем гравитацию
         ApplyGravity();
     }
 
     private void ApplyGravity()
     {
+        // Если персонаж стоит на земле и его вертикальная скорость отрицательна,
+        // устанавливаем минимально допустимую скорость падения
         if (_isGrounded && _velocity.y < 0)
         {
             _velocity.y = -2f;
         }
 
+        // Учитываем силу тяжести
         _velocity.y += _jumpController.Gravity * Time.deltaTime;
+
+        // Перемещаем персонажа с учётом текущей скорости
         _characterController.Move(_velocity * Time.deltaTime);
     }
 }
